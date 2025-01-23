cca67f3a6fda83b561b334cceb1d0a932df48dc5
bilan604 <bilan604@yahoo.com>
2025-01-23 01:22:24 -0500

    add files

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import redirect, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
