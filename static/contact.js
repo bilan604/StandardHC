74c72b9f97ec72927ec093107f275f0333a0b2b8
bilan604 <bilan604@yahoo.com>
2025-04-14 14:22:17 -0400

    create retro version

--- static/contact.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Script loaded!");
-
-    document.querySelector(".submit-btn").addEventListener("click", (event) => {
-        event.preventDefault();
-        alert("Your message has been sent!");
-    });
-});
